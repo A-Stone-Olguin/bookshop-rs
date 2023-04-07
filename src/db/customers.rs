64849dbd5c25d7e8de401d2c1793ae981ce91de8
Stone Olguin <olguinstone@gmail.com>
2023-04-07 12:24:33 -0600

    Added input sanitization for customers db

--- src/db/customers.rs
+++ src/db/customers.rs
@@ -1,23 +1,26 @@
 use super::db::connect;
+use log::info;
 
 pub fn create_customer(name: String, address: String) -> i64 {
     let db = connect();
-    db.execute(
-        "INSERT INTO customers (name, shippingAddress, accountBalance) VALUES (?1, ?2, 5.0)",
-        &[&name, &address],
-    )
-    .expect("expected to be able to insert into Customers table");
+    // Default balance of 5 dollars is added
+    let query = "INSERT INTO customers (name, shippingAddress, accountBalance) VALUES (:name, :address, 5.00)";
+    let mut stmt = db.prepare(query).expect("expected to prepare statement correctly");
+    stmt.execute(&[(":name", &name), (":address", &address)])
+        .expect("expected to be able to insert into Customers table");
+    info!(target: "file", "Successfully created customer: {}, Address: {}", name, address);
 
+    // TODO Why return statement? will check in future
     return get_customer_id(name, address);
 }
 
 pub fn get_customer_id(name: String, address: String) -> i64 {
     let db = connect();
-    let mut stmt = db
-        .prepare("SELECT id FROM customers WHERE name = ?1 AND shippingAddress = ?2")
-        .expect("expected to be able to select from Customers table");
+    let query = "SELECT id FROM customers WHERE name = :name AND shippingAddress = :address";
+    let mut stmt = db.prepare(query).expect("expected to be able to select from Customers table");
+
     let mut rows = stmt
-        .query_map(&[&name, &address], |row| row.get(0))
+        .query_map(&[(":name", &name), (":address", &address)], |row| row.get(0))
         .expect("expected to be able to get id from Customers table");
     let id = rows.next().unwrap().unwrap();
     return id;
@@ -25,33 +28,35 @@ pub fn get_customer_id(name: String, address: String) -> i64 {
 
 pub fn get_customer_address(cid: i64) -> String {
     let db = connect();
-    let mut stmt = db
-        .prepare("SELECT shippingAddress FROM customers WHERE id = ?1")
-        .expect("expected to be able to select from Customers table");
+    let query = "SELECT shippingAddress FROM customers WHERE id = :cid";
+    let mut stmt = db.prepare(query).expect("expected to be able to select from Customers table");
+
     let mut rows = stmt
-        .query_map(&[&cid], |row| row.get(0))
+        .query_map(&[(":cid", &format!("{}",cid))], |row| row.get(0))
         .expect("expected to be able to get shippingAddress from Customers table");
     let address = rows.next().unwrap().unwrap();
+    info!(target: "file", "Successfully retrieved cid {}'s address: {}", cid, address);
     return address;
 }
 
 pub fn update_customer_address(cid: i64, address: String) {
     let db = connect();
-    db.execute(
-        "UPDATE customers SET shippingAddress = ?1 WHERE id = ?2",
-        &[&address, &cid.to_string()],
-    )
-    .expect("expected to be able to update Customers table");
+    let query = "UPDATE customers SET shippingAddress = :address WHERE id = :cid";
+    let mut stmt = db.prepare(query).expect("expected to be able to update Customers table");
+    stmt.execute(&[(":address", &address), (":cid", &format!("{}",cid))]).expect("expected to be able to update Customers table");
+    info!(target: "file", "Successfully updated address of cid {} to {}", cid, address);
 }
 
+// TODO change name to get_customer_balance
 pub fn customer_balance(cid: i64) -> f64 {
     let db = connect();
-    let mut stmt = db
-        .prepare("SELECT accountBalance FROM customers WHERE id = ?1")
-        .expect("expected to be able to select from Customers table");
+    let query = "SELECT accountBalance FROM customers WHERE id = :cid";
+    let mut stmt = db.prepare(query).expect("expected to be able to select from Customers table");
+
     let mut rows = stmt
-        .query_map(&[&cid], |row| row.get(0))
+        .query_map(&[(":cid", &format!("{}",cid))], |row| row.get(0))
         .expect("expected to be able to get accountBalance from Customers table");
     let balance = rows.next().unwrap().unwrap();
+    info!(target: "file", "Successfully retrieved cid {}'s balance: {}", cid, balance);
     return balance;
 }
