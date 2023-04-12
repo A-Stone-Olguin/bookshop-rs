459746acb1354080fb1c0565bfb0ae8412e4d30e
Stone Olguin <olguinstone@gmail.com>
2023-04-12 15:34:26 -0600

    Now get_balance outputs only balance

--- src/handlers/customers.rs
+++ src/handlers/customers.rs
@@ -1,7 +1,7 @@
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
 
-use crate::db::customers;
+use crate::db::customers::{self, get_customer_id};
 use log::error;
 
 // TODO Add a way to check customer id function (Or print it)
@@ -40,36 +40,35 @@ pub fn create_customer(customer: Json<Customer>) -> Result<(), String> {
     Ok(())
 }
 
-// TODO change to require customer name not cid.
 #[put("/updateAddress", data = "<customer>")]
 pub fn update_address(customer: Json<Customer>) -> Result<(), String> {
-    let cid = match customer.id.clone() {
-        Some(i) => i,
-        None => return Err("No id provided".to_string()),
+    let mut name = match customer.name.clone() {
+        Some(n) => n,
+        None => return Err("No name provided".to_string()),
     };
     let mut address = match customer.shipping_address.clone() {
         Some(a) => a,
-        None => return Err("No address provided".to_string()),
+        None => return Err("No shipping_address provided".to_string()),
     };
     // Remove leading and trailing whitespace
-    address = address.trim().to_string(); 
+    name = name.trim().to_string(); 
+    address = address.trim().to_string();
 
-    match cid >= 0 {
-        true => 0, // Correct return code
-        false => return Err("Not a valid customer id. Must be >= 0".to_string()),
-    };
-    match validate_alphanumeric_input(address.clone(), "address".to_string(), "update_address".to_string()) {
+    match validate_alphanumeric_input(name.clone(), "name".to_string(), "update_address".to_string()) {
         Ok(()) => 0,
         Err(err_msg) => return Err(err_msg),
     };
-
+    match validate_alphanumeric_input(address.clone(), "shipping_address".to_string(), "update_address".to_string()) {
+        Ok(()) => 0,
+        Err(err_msg) => return Err(err_msg),
+    };
+    let cid = get_customer_id(name, address.clone());
     customers::update_customer_address(cid, address);
     Ok(())
 }
 
-// TODO change to only output balance
 #[get("/balance", format = "json", data = "<customer>")]
-pub fn get_balance(customer: Json<Customer>) -> Result<Json<Customer>, String> {
+pub fn get_balance(customer: Json<Customer>) -> Result<String, String> {
     let mut name = match customer.name.clone() {
         Some(n) => n,
         None => return Err("No name provided".to_string()),
@@ -91,14 +90,11 @@ pub fn get_balance(customer: Json<Customer>) -> Result<Json<Customer>, String> {
         Err(err_msg) => return Err(err_msg),
     };
 
-    let cid = customers::get_customer_id(name, address);
+    let cid = customers::get_customer_id(name.clone(), address);
     let balance = customers::get_customer_balance(cid);
-    Ok(Json(Customer {
-        id: None,
-        name: None,
-        shipping_address: None,
-        account_balance: Some(balance),
-    }))
+
+    let result_string = format!("Customer {} has balance: {}", name, balance);
+    Ok(result_string)
 }
 
 fn validate_alphanumeric_input(input : String, field : String, function: String) -> Result<(), String>{
